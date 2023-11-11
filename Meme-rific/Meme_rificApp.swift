030c8fc440cadb5cba8ce74881f6d2534197044f
ericz02 <ezheng0720@gmail.com>
2023-11-10 22:29:46 -0500

    Initial Commit

---
+++ Meme-rific/Meme_rificApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meme_rificApp.swift
+//  Meme-rific
+//
+//  Created by Eric Zheng on 11/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Meme_rificApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
